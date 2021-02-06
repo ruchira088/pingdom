6c858ab10099790551ecbdda2bb6523d3b0eab2d
Ruchira (Richie) Jayasekara <me@ruchij.com>
2021-02-06 17:15:21 +1100

    Implementing the DoobiePermissionDao

--- src/main/scala/com/ruchij/daos/permission/models/Permission.scala
+++ src/main/scala/com/ruchij/daos/permission/models/Permission.scala
@@ -3,9 +3,9 @@ package com.ruchij.daos.permission.models
 import org.joda.time.DateTime
 
 case class Permission(
-  userId: String,
   createdAt: DateTime,
   modifiedAt: DateTime,
+  userId: String,
   accountId: String,
   permissionType: PermissionType,
   grantedBy: Option[String]
