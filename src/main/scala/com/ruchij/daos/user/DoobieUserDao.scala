0911d79507922603208dfc15ba5aac9d59edfe95
Ruchira (Richie) Jayasekara <me@ruchij.com>
2021-02-06 17:08:52 +1100

    Implementing the DoobieUserDao

---
+++ src/main/scala/com/ruchij/daos/user/DoobieUserDao.scala
@@ -0,0 +1,29 @@
+package com.ruchij.daos.user
+
+import com.ruchij.daos.user.models.{Email, User}
+import com.ruchij.daos.doobie.DoobieCustomMappings._
+import doobie.ConnectionIO
+import doobie.implicits.toSqlInterpolator
+
+object DoobieUserDao extends UserDao[ConnectionIO] {
+  override def save(user: User): ConnectionIO[Int] =
+    sql"""
+      INSERT INTO user (id, created_at, modified_at, account_id, first_name, last_name, email)
+        VALUES (
+          ${user.id},
+          ${user.createdAt},
+          ${user.modifiedAt},
+          ${user.accountId},
+          ${user.firstName},
+          ${user.lastName},
+          ${user.email}
+        )
+    """
+      .update
+      .run
+
+  override def findByEmail(email: Email): ConnectionIO[Option[User]] =
+    sql"SELECT id, created_at, modified_at, account_id, first_name, last_name, email FROM user where email = $email"
+      .query[User]
+      .option
+}
