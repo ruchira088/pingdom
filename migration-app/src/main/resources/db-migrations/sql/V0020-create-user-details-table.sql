69bff064e4478523956aa6be52745ed9fad54588
Ruchira (Richie) Jayasekara <me@ruchij.com>
2021-02-09 19:21:38 +1100

    Creating the permission table

--- migration-app/src/main/resources/db-migrations/sql/V0020-create-user-details-table.sql
+++ migration-app/src/main/resources/db-migrations/sql/V0020-create-user-details-table.sql
@@ -2,13 +2,13 @@
 --changeset ruchira:create-user-details-table
 
 CREATE TABLE user_details (
-  id VARCHAR(64) PRIMARY KEY,
-  created_at TIMESTAMP NOT NULL,
-  modified_at TIMESTAMP NOT NULL,
-  account_id VARCHAR(64) NOT NULL,
-  first_name VARCHAR(128) NOT NULL,
-  last_name VARCHAR(256) NOT NULL,
-  email VARCHAR(256) UNIQUE NOT NULL,
+    id VARCHAR(64) PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    modified_at TIMESTAMP NOT NULL,
+    account_id VARCHAR(64) NOT NULL,
+    first_name VARCHAR(128) NOT NULL,
+    last_name VARCHAR(256) NOT NULL,
+    email VARCHAR(256) UNIQUE NOT NULL,
 
-  CONSTRAINT fk_user_details_account_id FOREIGN KEY (account_id) REFERENCES account(id)
+    CONSTRAINT fk_user_details_account_id FOREIGN KEY (account_id) REFERENCES account(id)
 );
---
+++ migration-app/src/main/resources/db-migrations/sql/V0040-create-permission-table.sql
@@ -0,0 +1,15 @@
+--liquibase formatted sql
+--changeset ruchira:create-permission-table
+
+CREATE TABLE permission (
+    user_id VARCHAR(64) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    modified_at TIMESTAMP NOT NULL,
+    account_id VARCHAR(64) NOT NULL,
+    permission_type VARCHAR(32) NOT NULL,
+    granted_by VARCHAR(64) NULL,
+
+    CONSTRAINT fk_permission_user_id FOREIGN KEY (user_id) REFERENCES user_details(id),
+    CONSTRAINT fk_permission_account_id FOREIGN KEY (account_id) REFERENCES account(id),
+    CONSTRAINT fk_permission_granted_ay FOREIGN KEY (granted_by) REFERENCES user_details(id)
+);
