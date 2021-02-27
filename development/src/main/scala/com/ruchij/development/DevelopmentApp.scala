9677244731dd174281c8bcdbd180259a3d7af545
Ruchira (Richie) Jayasekara <me@ruchij.com>
2021-02-27 21:07:09 +1100

    Creating the DevelopmentApp

---
+++ development/src/main/scala/com/ruchij/development/DevelopmentApp.scala
@@ -0,0 +1,9 @@
+package com.ruchij.development
+
+import cats.effect.{ExitCode, IO, IOApp}
+
+object DevelopmentApp extends IOApp {
+
+  override def run(args: List[String]): IO[ExitCode] = ???
+
+}
